053ff5003514bcd055605317f224156416c59316
Steve <fishesjourney@gmail.com>
2025-08-03 18:14:26 +0100

    practicing with django tags

--- my_app/views.py
+++ my_app/views.py
@@ -5,7 +5,8 @@ def example_view(request):
     return render(request, 'my_app/example.html')
 
 def variable_view(request):
-    my_var = {'first_name': 'rosolind', 'last_name': 'franklin',
-              'some_list':[1,2,3], 'some_dict':{'inside_key':'inside_value'}}
+    my_var = {'first_name': 'tim', 'last_name': 'minchin',
+    'some_list':[1,2,3], 'some_dict':{'inside_key':'inside_value'},
+    'list_with_spaces': 'This String Had Spaces Before The Cut Filter'}
 
     return render(request,'my_app/variable.html', context=my_var)
